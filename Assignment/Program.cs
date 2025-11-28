accb9615fa50e92fa960756ab355358b2022e71f
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-11-28 19:22:24 +0200

    Loading related data (Eager data)

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            
+            var context = new ITIContext();
+            var courses = context.Courses.FirstOrDefault(x => x.ID == 3);
+            if (courses != null)
+            {
+                
+                Console.WriteLine("Name: " + courses.Name +
+                    "\nDescription: " + courses.Description +
+                    "\nDuration: " + courses.Duration);
+
+                var department = context.Departments.FirstOrDefault(d => d.ID == courses.ID);
+                Console.WriteLine("Department: " + department.Name);
+                context.SaveChanges();
+            }
         }
     }
 }
